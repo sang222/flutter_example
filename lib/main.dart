85b393f5b4f6e8a84948962858a96a78d8b16b0b
SangDang <quangsang222@gmail.com>
2020-04-23 11:37:26 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,15 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
+void main() => runApp(XylophoneApp());
 
-void main() {
-    return runApp(
-        MaterialApp(
-            home: Scaffold(
-                backgroundColor: Colors.red,
-                appBar: AppBar(
-                    title: Text('Dicee'),
-                    backgroundColor: Colors.red,
-                ),
-                body: DicePage(),
-            ),
-        ),
-    );
-}
-
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-    int leftDiceNumber = 1;
-    int rightDiceNumber = 1;
-
-    void changeDiceFace() {
-        setState(() {
-            rightDiceNumber = Random().nextInt(6) + 1;
-            leftDiceNumber = Random().nextInt(6) + 1;
-        });
-    }
-
+class XylophoneApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        return Center(
-            child: Row(
-                children: <Widget>[
-                    Expanded(
-                        child: FlatButton(
-                            onPressed: changeDiceFace,
-                            child: Image.asset('images/dice$leftDiceNumber.png')
-                        )
-                    ),
-                    Expanded(
-                        child: FlatButton(
-                            onPressed: changeDiceFace,
-                            child: Image.asset('images/dice$rightDiceNumber.png')
-                        )
-                    )
-                ],
+        return MaterialApp(
+            home: Scaffold(
+                body: SafeArea(
+                    child: Container(),
+                ),
             ),
         );
     }
