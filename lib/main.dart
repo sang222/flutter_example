e3d403313c28f5a3f7243cbc0ff286b89d7670ca
SangDang <quangsang222@gmail.com>
2020-04-24 06:40:11 +0700

    init branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,97 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audio_cache.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(Quizzler());
 
-class XylophoneApp extends StatelessWidget {
-
-    void playSound(int soundNumber) {
-        final player = AudioCache();
-        player.play('note$soundNumber.wav');
-    }
-
-    Expanded buildKey({Color color, int soundNumber}) {
-        return Expanded(
-            child: FlatButton(
-                color: color,
-                onPressed: () {
-                    playSound(soundNumber);
-                }
-            ),
-        );
-    }
-
+class Quizzler extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
             home: Scaffold(
+                backgroundColor: Colors.grey.shade900,
                 body: SafeArea(
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                            buildKey(color: Colors.red, soundNumber: 1),
-                            buildKey(color: Colors.orange, soundNumber: 2),
-                            buildKey(color: Colors.yellow, soundNumber: 3),
-                            buildKey(color: Colors.green, soundNumber: 4),
-                            buildKey(color: Colors.teal, soundNumber: 5),
-                            buildKey(color: Colors.blue, soundNumber: 6),
-                            buildKey(color: Colors.purple, soundNumber: 7),
-                        ],
-                    )
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: QuizPage(),
+                    ),
                 ),
             ),
         );
     }
 }
+
+class QuizPage extends StatefulWidget {
+    @override
+    _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+    @override
+    Widget build(BuildContext context) {
+        return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+                Expanded(
+                    flex: 5,
+                    child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Center(
+                            child: Text(
+                                'This is where the question text will go.',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 25.0,
+                                    color: Colors.white,
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: FlatButton(
+                            textColor: Colors.white,
+                            color: Colors.green,
+                            child: Text(
+                                'True',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20.0,
+                                ),
+                            ),
+                            onPressed: () {
+                                //The user picked true.
+                            },
+                        ),
+                    ),
+                ),
+                Expanded(
+                    child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: FlatButton(
+                            color: Colors.red,
+                            child: Text(
+                                'False',
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.white,
+                                ),
+                            ),
+                            onPressed: () {
+                                //The user picked false.
+                            },
+                        ),
+                    ),
+                ),
+                //TODO: Add a Row here as your score keeper
+            ],
+        );
+    }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
