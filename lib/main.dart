590cb2b25532766d65df0c38132c23ede4a6954e
SangDang <quangsang222@gmail.com>
2020-04-27 20:19:09 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'price_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Colors.lightBlue,
-        scaffoldBackgroundColor: Colors.white),
-      home: PriceScreen(),
-    );
+    return Container();
   }
 }
